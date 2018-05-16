321c4bea87ee39cbd4a75897d9aade520eb230c7
walter-lopes <walter.vlopes@gmail.com>
2018-05-16 20:57:37 -0300

    Criando a estrutura do repositório para usuário.

---
+++ ShareBook/ShareBook.Repository/Repository/User/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using ShareBook.Data.Model;
+
+namespace ShareBook.Repository
+{
+    public interface IUserRepository : IRepositoryGeneric<User>
+    {
+    }
+}
---
+++ ShareBook/ShareBook.Repository/Repository/User/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using ShareBook.Data;
+using ShareBook.Data.Model;
+
+namespace ShareBook.Repository.Repository
+{
+    public class UserRepository : RepositoryGeneric<User>,  IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+       : base(context)
+        {
+            _context = context;
+        }
+    }
+}
