7ba067c195ae5c4734167f96222ccbb3d87c392a
walter-lopes <walter.vlopes@gmail.com>
2018-05-16 21:16:57 -0300

    Criando estrutura básico de view model e services para  usuário.

---
+++ ShareBook/ShareBook.Service/User/UserService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ShareBook.Data;
+using ShareBook.Data.Common;
+using ShareBook.Data.Model;
+using ShareBook.Repository;
+using ShareBook.Repository.Infra;
+using ShareBook.VM.Common;
+using ShareBook.VM.User.In;
+using ShareBook.VM.User.Out;
+
+namespace ShareBook.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+
+        public UserService(IUserRepository userRepository, IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<ResultServiceVM> CreateUser(UserInVM userInVM)
+        {
+            User user = Mapper.Map<User>(userInVM);
+
+            ResultService resultService = new ResultService(new UserValidation().Validate(user));
+
+            _unitOfWork.BeginTransaction();
+
+            if (resultService.Success)
+            {
+                await _userRepository.InsertAsync(user);
+                _unitOfWork.Commit();
+            }
+
+            return Mapper.Map<ResultServiceVM>(resultService);
+        }
+
+        public Task<UserOutByIdVM> GetBookById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ShareBook/ShareBook.VM/User/Model/UserVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShareBook.VM.User.Model
+{
+    public class UserVM
+    {
+
+        public Guid Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
