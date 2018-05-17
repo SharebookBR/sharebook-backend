797ba547d8f58aad661b9cece4f6f00fc407e736
walter-lopes <walter.vlopes@gmail.com>
2018-05-16 21:31:19 -0300

    Criando a controller para conta de usuário.

---
+++ ShareBook/ShareBook.Api/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShareBook.Data.Entities.User.In;
+using ShareBook.Service;
+using ShareBook.VM.Common;
+using ShareBook.VM.User.In;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShareBook.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+
+        private readonly IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<ResultServiceVM> Post([FromBody]UserInVM  userInVM)
+        {
+            return await _userService.CreateUser(userInVM);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
