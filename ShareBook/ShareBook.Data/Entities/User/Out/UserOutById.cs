b9ce77a2ab73815039bac98ed29d78c2dad99d62
walter-lopes <walter.vlopes@gmail.com>
2018-05-16 21:23:09 -0300

    Adicionando entidade usuário e view model no auto mapper.

---
+++ ShareBook/ShareBook.Data/Entities/User/Out/UserOutById.cs
@@ -0,0 +1,9 @@
+﻿using ShareBook.Data.Common;
+using ShareBook.Data.Entities.User.Model;
+namespace ShareBook.Data.Entities.User.Out
+{
+    public class UserOutById : ResultService
+    {
+        UserModel user { get; set; }
+    }
+}
