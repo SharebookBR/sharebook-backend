ed4804b76cfa834310505b497c370ee561059b69
Elvis Veiga <elviswveiga@gmail.com>
2018-05-24 08:40:41 -0300

    Criando criptografia de senha e salt.

---
+++ ShareBook/ShareBook.Helper/Crypto/Salt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ShareBook.Helper.Crypto
+{
+    public class Salt
+    {
+        public static string Create()
+        {
+            byte[] randomBytes = new byte[128 / 8];
+            using (var generator = RandomNumberGenerator.Create())
+            {
+                generator.GetBytes(randomBytes);
+                return Convert.ToBase64String(randomBytes);
+            }
+        }
+    }
+}
--- ShareBook/ShareBook.Service/User/UserService.cs
+++ ShareBook/ShareBook.Service/User/UserService.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using FluentValidation;
 using ShareBook.Domain;
 using ShareBook.Domain.Common;
+using ShareBook.Helper.Crypto;
 using ShareBook.Repository;
 using ShareBook.Repository.Infra;
 using ShareBook.Service.Generic;
@@ -11,33 +12,67 @@ namespace ShareBook.Service
 {
     public class UserService : BaseService<User>, IUserService
     {
+        #region Public
         public UserService(IUserRepository userRepository, IUnitOfWork unitOfWork, IValidator<User> validator) : base(userRepository, unitOfWork, validator) { }
 
         public User GetByEmailAndPassword(User user)
         {
+            string decryptedPass = user.Password;
+
             user = _repository.Get()
-                .Where(e => e.Email.Equals(user.Email, StringComparison.InvariantCultureIgnoreCase) && e.Password.Equals(user.Password))
+                .Where(e => e.Email.Equals(user.Email, StringComparison.InvariantCultureIgnoreCase))
                 .Select(x => new User
                 {
                     Id = x.Id,
                     Email = x.Email,
+                    Password = x.Password,
+                    PasswordSalt = x.PasswordSalt
                 }).FirstOrDefault();
+
+            if (user != null)
+                user = GetUserByPassword(user, decryptedPass);
 
             return user;
         }
-
+         
         public override Result<User> Insert(User user)
         {
             var result = Validate(user);
 
-            if ( _repository.Any(x => x.Email == user.Email))
+            if (_repository.Any(x => x.Email == user.Email))
                 result.Messages.Add("Usuário já possui email cadastrado.");
 
             user.Email = user.Email.ToLowerInvariant();
             if (result.Success)
+            {
+                user = GetUserEncryptedPass(user);
                 result.Value = _repository.Insert(user);
+                user = UserCleanup(user);
+            }
 
             return result;
         }
+        #endregion
+
+        #region Private
+        private User GetUserByPassword(User user, string decryptedPass)
+        {
+            if (user.Password == Hash.Create(decryptedPass, user.PasswordSalt))
+                return UserCleanup(user);
+            return new User();
+        }
+        private User GetUserEncryptedPass(User user)
+        {
+            user.PasswordSalt = Salt.Create();
+            user.Password = Hash.Create(user.Password, user.PasswordSalt);
+            return user;
+        }
+        private static User UserCleanup(User user)
+        {
+            user.Password = string.Empty;
+            user.PasswordSalt = string.Empty;
+            return user;
+        }
+        #endregion
     }
 }
