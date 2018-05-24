121af1db095bc788d597a67dad8efcbb18b8bbf5
Elvis Veiga <elviswveiga@gmail.com>
2018-05-24 15:03:44 -0300

    Ajuste de código

--- ShareBook/ShareBook.Service/User/UserService.cs
+++ ShareBook/ShareBook.Service/User/UserService.cs
@@ -57,7 +57,7 @@ namespace ShareBook.Service
         #region Private
         private bool IsValidPassword(User user, string decryptedPass)
         {
-            return (user != null && user.Password == Hash.Create(decryptedPass, user.PasswordSalt));
+            return user?.Password == Hash.Create(decryptedPass, user.PasswordSalt);
         }
         private User GetUserEncryptedPass(User user)
         {
