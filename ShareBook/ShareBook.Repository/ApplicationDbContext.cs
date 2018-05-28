6f23f727031e5ffee8cd9fea2f901ffc1a3ad285
Cussa <pablo.cussa@myq-solution.com>
2018-05-28 16:30:35 +0200

    Removendo o método de log para outra classe

--- ShareBook/ShareBook.Repository/ApplicationDbContext.cs
+++ ShareBook/ShareBook.Repository/ApplicationDbContext.cs
@@ -12,8 +12,6 @@ namespace ShareBook.Repository
 {
     public class ApplicationDbContext : DbContext
     {
-        private readonly List<EntityState> entityStates = new List<EntityState>() { EntityState.Added, EntityState.Modified, EntityState.Deleted };
-
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public ApplicationDbContext() { }
 
@@ -31,28 +29,7 @@ namespace ShareBook.Repository
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
         {
-            var logTime = DateTime.Now;
-
-            Guid? user = null;
-            if (!string.IsNullOrEmpty(Thread.CurrentPrincipal?.Identity?.Name))
-                user = new Guid(Thread.CurrentPrincipal?.Identity?.Name);
-
-            var changes = ChangeTracker.Entries()
-                .Where(x => entityStates.Contains(x.State) && x.Entity.GetType().IsSubclassOf(typeof(BaseEntity)))
-                .Select(t => new LogEntry()
-                {
-                    EntityName = t.Entity.GetType().Name,
-                    EntityId = new Guid(t.CurrentValues["Id"].ToString()),
-                    LogDateTime = logTime,
-                    Operation = t.State.ToString(),
-                    UserId = user,
-                    OriginalValues = string.Join("\n", t.OriginalValues.Properties.ToDictionary(pn => pn.Name, pn => t.OriginalValues[pn])),
-                    UpdatedValues = string.Join("\n", t.CurrentValues.Properties.ToDictionary(pn => pn.Name, pn => t.CurrentValues[pn])),
-                })
-                .ToList();
-
-            LogEntries.AddRange(changes);
-
+            this.LogChanges();
             return base.SaveChangesAsync(cancellationToken);
         }
     }
