541314631d313c2e32f82545ae5b3213b9d3ab66
Cussa <cussa.mitre@gmail.com>
2018-05-28 23:56:44 +0200

    Adicinando operações CRUD direto no BaseController

--- ShareBook/ShareBook.Api/Controllers/BaseController.cs
+++ ShareBook/ShareBook.Api/Controllers/BaseController.cs
@@ -1,10 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ShareBook.Api.Filters;
+using ShareBook.Domain.Common;
+using ShareBook.Service.Generic;
+using System;
+using System.Linq.Expressions;
 
 namespace ShareBook.Api.Controllers
 {
     [GetClaimsFilter]
-    public class BaseController : Controller
+    public class BaseController<T> : Controller where T : BaseEntity
     {
+        protected readonly IBaseService<T> _service;
+        private Expression<Func<T, object>> _defaultOrder = x => x.Id;
+
+        public BaseController(IBaseService<T> service)
+        {
+            _service = service;
+        }
+        protected void SetDefault(Expression<Func<T, object>> defaultOrder)
+        {
+            _defaultOrder = defaultOrder;
+        }
+
+        [HttpGet()]
+        public PagedList<T> GetAll() => GetPaged(1, 15);
+
+        [HttpGet("{page}/{items}")]
+        public PagedList<T> GetPaged(int page, int items) => _service.Get(x => true, _defaultOrder, page, items);
+
+        [HttpGet("{id}")]
+        public T GetById(string id) => _service.Get(new Guid(id));
+
+        [Authorize("Bearer")]
+        [HttpPost]
+        public Result<T> Create([FromBody]T entity) => _service.Insert(entity);
+
+        [Authorize("Bearer")]
+        [HttpPut]
+        public Result<T> Update([FromBody]T entity) => _service.Update(entity);
+
+        [Authorize("Bearer")]
+        [HttpDelete("{id}")]
+        public Result Delete(string id) => _service.Delete(new Guid(id));
     }
 }
--- ShareBook/ShareBook.Api/Controllers/BookController.cs
+++ ShareBook/ShareBook.Api/Controllers/BookController.cs
@@ -1,41 +1,15 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using ShareBook.Domain;
-using ShareBook.Domain.Common;
 using ShareBook.Service;
-using System;
 
 namespace ShareBook.Api.Controllers
 {
     [Route("api/[controller]")]
-    public class BookController : BaseController
+    public class BookController : BaseController<Book>
     {
-        private readonly IBookService _bookService;
-
-        public BookController(IBookService bookService)
+        public BookController(IBookService bookService) : base(bookService)
         {
-            _bookService = bookService;
+            SetDefault(x => x.Name);
         }
-
-        [HttpGet()]
-        public PagedList<Book> GetBooks() => GetBooksPaged(1, 15);
-
-        [HttpGet("{page}/{items}")]
-        public PagedList<Book> GetBooksPaged(int page, int items) => _bookService.Get(x => true, x => x.Name, page, items);
-
-        [HttpGet("{id}", Name = "Get")]
-        public Book GetBookById(string id) => _bookService.Get(new Guid(id));
-
-        [Authorize("Bearer")]
-        [HttpPost]
-        public Result<Book> CreateBook([FromBody]Book book) => _bookService.Insert(book);
-
-        [Authorize("Bearer")]
-        [HttpPut]
-        public Result<Book> UpdateBook([FromBody]Book book) => _bookService.Update(book);
-
-        [Authorize("Bearer")]
-        [HttpDelete("{id}")]
-        public Result UpdateBook(string id) => _bookService.Delete(new Guid(id));
     }
 }
