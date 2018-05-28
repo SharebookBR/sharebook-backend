b23b2a6b2fb267e6773b4dea98d37198879c5bdc
walter-lopes <walter_vinicius@outlook.com>
2018-05-28 14:14:38 -0300

    Implementando o serviço de e-mail para a apalicação.

---
+++ ShareBook/ShareBook.Service/Email/EmailService.cs
@@ -0,0 +1,44 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace ShareBook.Service.Email
+{
+    public class EmailService : IEmailService
+    {
+        public void Send(string emailRecipient, string nameRecipient, string messageText, string subject)
+        {
+            var message = FormatEmail(emailRecipient, nameRecipient, messageText, subject);
+
+            using (var client = new SmtpClient())
+            {
+                // TODO - passar os valores por variaveis de ambiente, ou arquivos de configuração...
+                string hostSMTP = string.Empty;
+                string userNameSMTP = string.Empty;
+                string passwordSMTP = string.Empty;
+
+               
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                client.Connect(hostSMTP, 587, false);
+
+              
+                client.Authenticate(userNameSMTP, passwordSMTP);
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+
+        private MimeMessage FormatEmail(string emailRecipient, string nameRecipient, string messageText, string subject)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(nameRecipient, emailRecipient));
+            message.Subject = subject;
+            message.Body = new TextPart("plain")
+            {
+                Text = messageText
+            };
+            return message;
+        }
+    }
+}
---
+++ ShareBook/ShareBook.Service/Email/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace ShareBook.Service.Email
+{
+    public interface IEmailService
+    {
+        void Send(string emailRecipient, string nameRecipient, string messageText, string subject);
+    }
+}
