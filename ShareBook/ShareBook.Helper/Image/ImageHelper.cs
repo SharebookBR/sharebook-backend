fe826669d69ab209de35d03409baad4bd5e60971
walter-lopes <walter_vinicius@outlook.com>
2018-06-10 18:49:52 -0300

    refactor - feature do c# 7

--- ShareBook/ShareBook.Helper/Image/ImageHelper.cs
+++ ShareBook/ShareBook.Helper/Image/ImageHelper.cs
@@ -8,9 +8,6 @@ namespace ShareBook.Helper.Image
     public static class ImageHelper
     {
         public static string FormatImageName(string originalName, string newName)
-        {
-            return originalName.Replace(Path.GetFileNameWithoutExtension(originalName), newName);
-        }
-
+            => originalName.Replace(Path.GetFileNameWithoutExtension(originalName), newName);
     }
 }
