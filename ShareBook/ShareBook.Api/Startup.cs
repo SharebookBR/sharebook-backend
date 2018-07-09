17ba0fe36a26759220a1c5deeed91e97f923b94b
Raffaello Damgaard <raffacabofrio@gmail.com>
2018-07-09 19:32:15 -0300

    Ambiente Stage.

--- ShareBook/ShareBook.Api/Startup.cs
+++ ShareBook/ShareBook.Api/Startup.cs
@@ -74,11 +74,8 @@ namespace ShareBook.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseExceptionHandlerMiddleware();
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseExceptionHandlerMiddleware();
 
             app.UseStaticFiles();
 
