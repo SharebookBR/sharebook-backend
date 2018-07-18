3c997b88876d11934c3b9990a29372a56e3c4005
Raffaello Damgaard Neves <raffacabofrio@gmail.com>
2018-07-18 17:40:43 -0300

    Fake fail test

--- ShareBook/ShareBook.Test.Unit/Services/EmailTemplateTests.cs
+++ ShareBook/ShareBook.Test.Unit/Services/EmailTemplateTests.cs
@@ -68,6 +68,7 @@ namespace ShareBook.Test.Unit.Services
             var result = emailTemplate.GenerateHtmlFromTemplateAsync("NewBookInsertedTemplate", vm).Result;
             //<!DOCTYPE html>\r\n<html lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\">\r\n<head>\r\n    <meta charset=\"utf-8\" />\r\n    <title>Novo livro cadastrado - Sharebook</title>\r\n</head>\r\n<body>\r\n    <p>\r\n        Olá Cussa Mitre,\r\n    </p>\r\n    <p>\r\n        Um novo livro foi cadastrado. Veja mais informações abaixo:\r\n    </p>\r\n\r\n    <ul>\r\n        <li><strong>Livro: </strong>Lord of the Rings</li>\r\n        <li><strong>Autor: </strong>J. R. R. Tolkien</li>\r\n        <li><strong>Usuário: </strong>Rodrigo</li>\r\n    </ul>\r\n\r\n    <p>Sharebook</p>\r\n</body>\r\n</html>
 
+            Assert.Contains("FAKE FAIL TEST. SHOULD FAIL DEPLOY.", result);
             Assert.Contains("Olá Cussa Mitre,", result);
             Assert.Contains("<li><strong>Livro: </strong>Lord of the Rings</li>", result);
             Assert.Contains("<li><strong>Autor: </strong>J. R. R. Tolkien</li>", result);
