0f4641df46a5809c7b2cb4a466ad3601f41e44b3
Cussa <cussa.mitre@gmail.com>
2018-07-22 15:57:49 +0200

    Criação do BaseViewModel

---
+++ ShareBook/ShareBook.Api/ViewModels/BaseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ShareBook.Api.ViewModels
+{
+    public abstract class BaseViewModel
+    {
+        [JsonIgnore]
+        public Guid Id { get; set; }
+    }
+}
