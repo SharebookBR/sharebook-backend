df8cf4271713e2db8dd7d1d89726639404304e70
walter-lopes <walter_vinicius@outlook.com>
2018-07-25 14:59:00 -0300

    criando bases controllers genericas

---
+++ ShareBook/ShareBook.Api/ViewModels/UpdateBookVM.cs
@@ -0,0 +1,25 @@
+﻿using ShareBook.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShareBook.Api.ViewModels
+{
+    public class UpdateBookVM : BaseViewModel
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public bool sadasdasdasd { get; set; }
+
+        public string ImageName { get; set; }
+
+        public byte[] ImageBytes { get; set; }
+
+        public FreightOption FreightOption { get; set; }
+    }
+}
