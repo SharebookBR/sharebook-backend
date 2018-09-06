2a6420590e82eb89d39f2c4d543f28e2aba27395
walter-lopes <walter_vinicius@outlook.com>
2018-09-06 16:27:29 -0300

    modificando endpoint do book controller para aceitar o motivo do interesse na doação

---
+++ ShareBook/ShareBook.Api/ViewModels/RequestBookVM.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ShareBook.Api.ViewModels
+{
+    public class RequestBookVM
+    {
+        public Guid BookId { get; set; }
+
+        public string Reason { get; set; }
+    }
+}
