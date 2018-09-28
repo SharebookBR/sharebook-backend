aef1d4908f83a8ab8807bdda0487e87fae6c2df5
walter-lopes <walter_vinicius@outlook.com>
2018-09-28 17:32:02 -0300

    criando a entidade address e realizando o mapeamento para entidade usuário

--- ShareBook/ShareBook.Api/AutoMapper/ViewModelToDomainMappingProfile.cs
+++ ShareBook/ShareBook.Api/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -19,7 +19,15 @@ namespace ShareBook.Api.AutoMapper
             #region [ User ]
             CreateMap<LoginUserVM, User>();
 
-            CreateMap<RegisterUserVM, User>();
+            CreateMap<RegisterUserVM, User>()
+                 .ForMember(dest => dest.Address.Street, opt => opt.MapFrom(src => src.Street))
+                 .ForMember(dest => dest.Address.Number, opt => opt.MapFrom(src => src.Number))
+                 .ForMember(dest => dest.Address.PostalCode, opt => opt.MapFrom(src => src.PostalCode))
+                 .ForMember(dest => dest.Address.State, opt => opt.MapFrom(src => src.State))
+                 .ForMember(dest => dest.Address.City, opt => opt.MapFrom(src => src.City))
+                 .ForMember(dest => dest.Address.Neighborhood, opt => opt.MapFrom(src => src.Neighborhood))
+                 .ForMember(dest => dest.Address.Country, opt => opt.MapFrom(src => src.Complement))
+                 .ForMember(dest => dest.Address.Complement, opt => opt.MapFrom(src => src.Complement));
 
             CreateMap<UpdateUserVM, User>();
             #endregion
