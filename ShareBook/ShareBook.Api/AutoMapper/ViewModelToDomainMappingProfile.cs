8b423214ec5f9690c45a00df4caba7f63558b130
Walter Cardoso <walter_vinicius@outlook.com>
2018-10-11 12:16:25 -0300

    corrigindo mapeamento do user

--- ShareBook/ShareBook.Api/AutoMapper/ViewModelToDomainMappingProfile.cs
+++ ShareBook/ShareBook.Api/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -26,7 +26,7 @@ namespace ShareBook.Api.AutoMapper
                  .ForPath(dest => dest.Address.State, opt => opt.MapFrom(src => src.State))
                  .ForPath(dest => dest.Address.City, opt => opt.MapFrom(src => src.City))
                  .ForPath(dest => dest.Address.Neighborhood, opt => opt.MapFrom(src => src.Neighborhood))
-                 .ForPath(dest => dest.Address.Country, opt => opt.MapFrom(src => src.Complement))
+                 .ForPath(dest => dest.Address.Country, opt => opt.MapFrom(src => src.Country))
                  .ForPath(dest => dest.Address.Complement, opt => opt.MapFrom(src => src.Complement));
 
             CreateMap<UpdateUserVM, User>();
