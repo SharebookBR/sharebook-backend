4940bb453901326736942f3c79409ed32bbbb2a7
walter-lopes <walter_vinicius@outlook.com>
2018-10-19 13:10:19 -0300

    criando estrutura para testes de integração

---
+++ ShareBook/ShareBook.Test.Integration/Base/IntegrationTestBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using ShareBook.Api;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace ShareBook.Test.Integration.Base
+{
+    public class IntegrationTestBase
+    {
+        public TestServer Server { get; set; }
+        public HttpClient Client { get; set; }
+
+        public IntegrationTestBase()
+        {
+            Server = new TestServer(new WebHostBuilder()
+                    .UseStartup<Startup>());
+            Client = Server.CreateClient();
+        }
+    }
+}
---
+++ ShareBook/ShareBook.Test.Integration/BookTests/BookControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using ShareBook.Test.Integration.Base;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShareBook.Test.Integration.BookTests
+{
+    public class BookControllerTest : IntegrationTestBase
+    {
+
+        public BookControllerTest()
+        {
+            //Falta startar o banco com dados...
+        }
+
+        [Fact]
+        public async Task ListTop15NewBook()
+        {
+            var response = await Client.GetAsync("api/Book/Top15NewBooks");
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
