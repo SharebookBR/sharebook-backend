f5ddd7e2239dc673d1d0b6ae1a06d4879b48063a
walter-lopes <walter_vinicius@outlook.com>
2018-10-25 16:44:38 -0300

    criando metodos que gera hashcode e método que valida a expiração desse hashcode. testes unitários também escritos.

---
+++ ShareBook/ShareBook.Test.Unit/Domain/UserTests.cs
@@ -0,0 +1,68 @@
+﻿using ShareBook.Domain;
+using ShareBook.Helper.Crypto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ShareBook.Test.Unit.Domain
+{
+    public class UserTests
+    {
+        [Fact]
+        public void HashCodePasswordDateExpired()
+        {
+            var expectedDay = DateTime.Now.AddDays(2);
+            var hashCodePassword = Salt.Create();
+            var user = new User()
+            {
+                HashCodePassword = hashCodePassword,
+                HashCodePasswordExpiryDate = expectedDay
+            };
+
+           Assert.False(user.HashCodePasswordIsValid(hashCodePassword));
+        }
+
+        [Fact]
+        public void HashCodePasswordDateValid()
+        {
+            var expectedDay = DateTime.Now.AddDays(1);
+            var hashCodePassword = Salt.Create();
+            var user = new User()
+            {
+                HashCodePassword = hashCodePassword,
+                HashCodePasswordExpiryDate = expectedDay
+            };
+
+            Assert.True(user.HashCodePasswordIsValid(hashCodePassword));
+        }
+
+        [Fact]
+        public void HashCodeInValid()
+        {
+            var expectedDay = DateTime.Now.AddDays(1);
+            var hashCodePassword = Salt.Create();
+            var user = new User()
+            {
+                HashCodePassword = hashCodePassword,
+                HashCodePasswordExpiryDate = expectedDay
+            };
+
+            Assert.False(user.HashCodePasswordIsValid("HASHCODEINVALID"));
+        }
+
+        [Fact]
+        public void GenerateHashCodePasswordValid()
+        {
+            var expectedDay = DateTime.Now.AddDays(1);
+            var hashCodePassword = Salt.Create();
+            var user = new User()
+            {
+                HashCodePassword = hashCodePassword,
+                HashCodePasswordExpiryDate = expectedDay
+            };
+
+            Assert.Equal(user.HashCodePasswordExpiryDate.Date, expectedDay.Date);
+        }
+    }
+}
