151ec2a1b0d952682282475b20e75be4c36b6460
Raffaello Damgaard <raffacabofrio@gmail.com>
2019-01-12 19:36:52 -0200

    Finalizando o job RemoveBookFromShowcase.

--- ShareBook/Sharebook.Jobs/Jobs/3 - RemoveBookFromShowcase.cs
+++ ShareBook/Sharebook.Jobs/Jobs/3 - RemoveBookFromShowcase.cs
@@ -10,18 +10,26 @@ namespace Sharebook.Jobs
     public class RemoveBookFromShowcase : GenericJob, IJob
     {
         private readonly IBookService _bookService;
+        private readonly IEmailService _emailService;
+        private readonly IEmailTemplate _emailTemplate;
 
-        public RemoveBookFromShowcase(IBookService bookService, IJobHistoryRepository jobHistoryRepo) : base(jobHistoryRepo)
+        public RemoveBookFromShowcase(
+            IBookService bookService, 
+            IJobHistoryRepository jobHistoryRepo,
+            IEmailService emailService,
+            IEmailTemplate emailTemplate) : base(jobHistoryRepo)
         {
 
             JobName     = "RemoveBookFromShowcase";
             Description = "Remove o livro da vitrine no dia da decisão. " +
                           "Caso o livro não tenha interessado o mesmo tem a data renovada por mais 10 dias.";
             Interval    = Interval.Dayly;
-            Active      = false;
+            Active      = true;
             BestTimeToExecute = new TimeSpan(9, 0, 0);
 
             _bookService = bookService;
+            _emailService = emailService;
+            _emailTemplate = emailTemplate;
         }
 
         public override JobHistory Work()
@@ -35,7 +43,11 @@ namespace Sharebook.Jobs
             foreach (var book in books)
             {
                 // Só trata livros disponíves
-                if (book.Status() != BookStatus.Available) continue;
+                if (book.Status() != BookStatus.Available)
+                {
+                    messages.Add(string.Format("Livro '{0}' não foi processado porque não está disponível.", book.Title));
+                    continue;
+                }
 
                 if (book.BookUsers.Count > 0)
                 {
@@ -46,6 +58,7 @@ namespace Sharebook.Jobs
                 {
                     book.ChooseDate = DateTime.Today.AddDays(10);
                     messages.Add(string.Format("Livro '{0}' vai ficar +10 dias na vitrine porque ainda não te interessados. :/", book.Title));
+                    SendEmail(book);
                 }
 
                 _bookService.Update(book);
@@ -58,5 +71,32 @@ namespace Sharebook.Jobs
                 Details = String.Join("\n", messages.ToArray())
             };
         }
+
+
+        #region métodos privados de apoio
+
+        private void SendEmail(Book book)
+        {
+            var emailSubject = "SHAREBOOK - RENOVAMOS SUA DOAÇÃO.";
+
+            var vm = new
+            {
+                DonorName = book.User.Name,
+                BookTitle = book.Title,
+                BookSlug = book.Slug,
+                FacilitatorName = book.UserFacilitator.Name,
+                FacilitatorEmail = book.UserFacilitator.Email,
+                FacilitatorWhatsapp = book.UserFacilitator.Phone,
+                FacilitatorLinkedin = book.UserFacilitator.Linkedin
+            };
+            var emailBodyHTML = _emailTemplate.GenerateHtmlFromTemplateAsync("ChooseDateRenewTemplate", vm).Result;
+
+            // TODO: não enviar cópia para admins quando o job já estiver bem testado e amadurecido.
+            var copyAdmins = true;
+            _emailService.Send(book.User.Email, book.User.Name, emailBodyHTML, emailSubject, copyAdmins);
+        }
+
+        #endregion
+
     }
 }
