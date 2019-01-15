3917f9723195ba6ff3e288ab9b8d0e2c97d79d9e
Bruno Henn <brunofhenn@gmail.com>
2019-01-15 10:39:38 -0500

    Adicionado o campo trackingNumber

--- ShareBook/ShareBook.Repository/Migrations/20190115153133_TrackinNumber.Designer.cs
+++ ShareBook/ShareBook.Repository/Migrations/20190115153133_TrackinNumber.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace ShareBook.Repository.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190115153133_TrackinNumber")]
+    partial class TrackinNumber
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
