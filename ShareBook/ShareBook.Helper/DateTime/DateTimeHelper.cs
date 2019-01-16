f526aa0ff014e2ac5fc45a82977a2fc00c57c1b2
Raffaello Damgaard <raffacabofrio@gmail.com>
2019-01-16 20:29:10 -0200

    Bugfix na verificação se é hora de executar o job.

---
+++ ShareBook/ShareBook.Helper/DateTime/DateTimeHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ShareBook.Helper
+{
+    static public class DateTimeHelper
+    {
+        static public TimeSpan GetTimeNowSaoPaulo()
+        {
+            var now = TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+            var today = new DateTime(now.Year, now.Month, now.Day);
+            return now - today;
+        }
+    }
+}
