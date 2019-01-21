e6f3cc9c6c789ea5d73177663cb452108017169d
Bruno Henn <brunofhenn@gmail.com>
2019-01-21 10:21:47 -0500

    Adicionado o trackingNumber nos endpoints que usam bookViewModel

--- ShareBook/ShareBook.Api/AutoMapper/DomainToViewModelMappingProfile.cs
+++ ShareBook/ShareBook.Api/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -22,7 +22,8 @@ namespace ShareBook.Api.AutoMapper
                  .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status().Description()))
                  .ForMember(dest => dest.CreationDate, opt => opt.MapFrom(src => src.CreationDate))
                  .ForMember(dest => dest.ChooseDate, opt => opt.MapFrom(src => src.ChooseDate))
-                 .ForMember(dest => dest.Winner, opt => opt.MapFrom(src => src.Winner()));
+                 .ForMember(dest => dest.Winner, opt => opt.MapFrom(src => src.Winner()))
+                 .ForMember(dest => dest.TrackingNumber, opt => opt.MapFrom(src => src.TrackingNumber));
 
             CreateMap<BookUser, MyBookRequestVM>()
                 .ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.Book.Author))
