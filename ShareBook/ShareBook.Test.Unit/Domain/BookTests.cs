51f4145b115b7c8142dfec6456f909521c2f71eb
Raffaello Damgaard <raffacabofrio@gmail.com>
2019-02-28 14:52:46 -0300

    Adequando o teste unitário ao timezone padrão São Paulo.

--- ShareBook/ShareBook.Test.Unit/Domain/BookTests.cs
+++ ShareBook/ShareBook.Test.Unit/Domain/BookTests.cs
@@ -1,5 +1,6 @@
 using ShareBook.Domain;
 using ShareBook.Domain.Enums;
+using ShareBook.Helper;
 using System;
 using System.Collections.Generic;
 using Xunit;
@@ -97,21 +98,21 @@ namespace ShareBook.Test.Unit.Domain
         [Fact]
         public void MayChooseWinnerWhenChooseDateIsYesterday()
         {
-            var book = new Book { ChooseDate = DateTime.Now.AddDays(-1) };
+            var book = new Book { ChooseDate = DateTimeHelper.GetTodaySaoPaulo().AddDays(-1) };
             Assert.True(book.MayChooseWinner());
         }
 
         [Fact]
         public void MayChooseWinnerWhenChooseDateIsToday()
         {
-            var book = new Book { ChooseDate = DateTime.Now };
+            var book = new Book { ChooseDate = DateTimeHelper.GetTodaySaoPaulo() };
             Assert.True(book.MayChooseWinner());
         }
 
         [Fact]
         public void MayNotChooseWinnerWhenChooseDateIsTomorrow()
         {
-            var book = new Book { ChooseDate = DateTime.Now.AddDays(1) };
+            var book = new Book { ChooseDate = DateTimeHelper.GetTodaySaoPaulo().AddDays(1) };
             Assert.False(book.MayChooseWinner());
         }
     }
