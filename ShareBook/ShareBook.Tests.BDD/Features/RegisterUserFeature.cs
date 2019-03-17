b683c40f0f5fb3db54143e900d67bfd490cefaf1
walter-lopes <walter_vinicius@outlook.com>
2019-03-17 14:19:34 -0300

    criando projeto bdd

---
+++ ShareBook/ShareBook.Tests.BDD/Features/RegisterUserFeature.cs
@@ -0,0 +1,36 @@
+﻿using LightBDD.Framework;
+using LightBDD.Framework.Scenarios;
+using LightBDD.XUnit2;
+using ShareBook.Tests.BDD.Configurations;
+
+[assembly: ConfiguredLightBddScope]
+namespace ShareBook.Tests.BDD.Features
+{
+	[Label("FEAT-1")]
+	[FeatureDescription(
+@"In order to 
+As a
+I want to ")]
+	public partial class RegisterUserFeature
+	{
+		[Label("SCENARIO-1")]
+		[Scenario]
+		public void Template_basic_scenario()
+		{
+			Runner.RunScenario(
+				Given_template_method,
+				When_template_method,
+				Then_template_method);
+		}
+
+		[Label("SCENARIO-1")]
+		[Scenario]
+		public void Template_extended_scenario()
+		{
+			Runner.RunScenario(
+				_ => Given_template_method(),
+				_ => When_template_method(),
+				_ => Then_template_method());
+		}
+	}
+}
