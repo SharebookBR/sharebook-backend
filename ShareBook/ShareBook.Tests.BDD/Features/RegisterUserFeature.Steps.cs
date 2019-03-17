4d06d0305fc537ec08e6893231372695e26e254c
walter-lopes <walter_vinicius@outlook.com>
2019-03-17 14:32:20 -0300

    criando steps para registrar usuário

--- ShareBook/ShareBook.Tests.BDD/Features/RegisterUserFeature.Steps.cs
+++ ShareBook/ShareBook.Tests.BDD/Features/RegisterUserFeature.Steps.cs
@@ -5,17 +5,17 @@ namespace ShareBook.Tests.BDD.Features
 {
 	public partial class RegisterUserFeature: FeatureFixture
 	{
-		private void Given_template_method()
+		private void Given_new_user_want_to_join_with_this_datas()
 		{
 			StepExecution.Current.IgnoreScenario("Not implemented yet");
 		}
 
-		private void When_template_method()
+		private void When_the_new_user_registers()
 		{
 			StepExecution.Current.IgnoreScenario("Not implemented yet");
 		}
 
-		private void Then_template_method()
+		private void Then_will_receive_a_success_message()
 		{
 			StepExecution.Current.IgnoreScenario("Not implemented yet");
 		}
--- ShareBook/ShareBook.Tests.BDD/Features/RegisterUserFeature.cs
+++ ShareBook/ShareBook.Tests.BDD/Features/RegisterUserFeature.cs
@@ -6,31 +6,19 @@ using ShareBook.Tests.BDD.Configurations;
 [assembly: ConfiguredLightBddScope]
 namespace ShareBook.Tests.BDD.Features
 {
-	[Label("FEAT-1")]
+	[Label("REGISTER-USER")]
 	[FeatureDescription(
-@"In order to 
-As a
-I want to ")]
+        @"New user wants to join in sharebook plataform")]
 	public partial class RegisterUserFeature
 	{
-		[Label("SCENARIO-1")]
+		[Label("Registration with all data correct")]
 		[Scenario]
 		public void Template_basic_scenario()
 		{
 			Runner.RunScenario(
-				Given_template_method,
-				When_template_method,
-				Then_template_method);
-		}
-
-		[Label("SCENARIO-1")]
-		[Scenario]
-		public void Template_extended_scenario()
-		{
-			Runner.RunScenario(
-				_ => Given_template_method(),
-				_ => When_template_method(),
-				_ => Then_template_method());
+                Given_new_user_want_to_join_with_this_datas,
+                When_the_new_user_registers,
+                Then_will_receive_a_success_message);
 		}
 	}
 }
