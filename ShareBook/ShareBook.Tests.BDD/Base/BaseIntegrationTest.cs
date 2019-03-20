237037e0bdc9d2276ada440299d9215448ef8d40
walter-lopes <walter_vinicius@outlook.com>
2019-03-20 10:16:52 -0300

    crinado base integration test

---
+++ ShareBook/ShareBook.Tests.BDD/Base/BaseIntegrationTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using ShareBook.Api;
+using System.Net.Http;
+
+namespace ShareBook.Tests.BDD.Base
+{
+    public class BaseIntegrationTest
+    {
+        public TestServer Server { get; set; }
+        public HttpClient Client { get; set; }
+
+        public BaseIntegrationTest()
+        {
+            Server = new TestServer(new WebHostBuilder()
+                    .UseStartup<Startup>());
+            Client = Server.CreateClient();
+        }
+    }
+}
