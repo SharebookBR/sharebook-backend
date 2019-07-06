caaed2a9bd13eab7f3d7733c43838e64ba08d044
Walter Vinicius <walter_vinicius@outlook.com>
2019-07-06 20:19:53 -0300

    migrate database

---
+++ ShareBook/ShareBook.Repository/Migrations/20190706214154_Upgrade_Nvarchar_To_Varchar.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShareBook.Repository.Migrations
+{
+    public partial class Upgrade_Nvarchar_To_Varchar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TrackingNumber",
+                table: "Books",
+                type: "varchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Books",
+                type: "varchar(max)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)",
+                oldMaxLength: 50);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TrackingNumber",
+                table: "Books",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Books",
+                type: "varchar(200)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(max)",
+                oldMaxLength: 50);
+        }
+    }
+}
