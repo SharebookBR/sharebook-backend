c3d52518d71a64e633f4b8acd9f23de4497a9fe2
Kalil Teixeira Ventura Monteiro <kalilventur@gmail.com>
2020-04-18 16:40:20 -0300

    Exclusao de variaveis desnecessarias

--- ShareBook/ShareBook.Api/Controllers/CategoryController.cs
+++ ShareBook/ShareBook.Api/Controllers/CategoryController.cs
@@ -4,16 +4,17 @@ using ShareBook.Domain;
 using ShareBook.Domain.Common;
 using ShareBook.Service;
 
-namespace ShareBook.Api.Controllers {
-    [Route ("api/[controller]")]
-    public class CategoryController : BaseCrudController<Category> {
-        private readonly IMapper _mapper;
-        public CategoryController (ICategoryService categoryService,
-            IMapper mapper) : base (categoryService, mapper) {
-            _mapper = mapper;
-            SetDefault (x => x.Name);
+namespace ShareBook.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoryController : BaseCrudController<Category>
+    {
+        public CategoryController(ICategoryService categoryService,
+            IMapper mapper) : base(categoryService, mapper)
+        {
+            SetDefault(x => x.Name);
         }
 
-        public override PagedList<Category> GetAll () => Paged (1, 50);
+        public override PagedList<Category> GetAll() => Paged(1, 50);
     }
 }
