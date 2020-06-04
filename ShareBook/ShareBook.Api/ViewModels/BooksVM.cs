92f92cae5b3ee96b6f260c94d00f6a11b4d32a8b
Raffaello Damgaard <raffacabofrio@gmail.com>
2020-06-04 17:07:32 -0300

    Ajustando alguns endpoints pra funcionar corretamente com o status único.

--- ShareBook/ShareBook.Api/ViewModels/BooksVM.cs
+++ ShareBook/ShareBook.Api/ViewModels/BooksVM.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 namespace ShareBook.Api.ViewModels
 {
-    public class BooksVM
+    public class BookVMAdm
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public string Winner { get; set; }
         public string Donor { get; set; }
+        public Guid? UserIdFacilitator { get; set; }
         public string Facilitator { get; set; }
         public string FacilitatorNotes { get; set; }
         public string PhoneDonor { get; set; }
@@ -17,5 +18,36 @@ namespace ShareBook.Api.ViewModels
         public string TrackingNumber { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime? ChooseDate { get; set; }
+        public string FreightOption { get; set; }
+        public Guid CategoryId { get; set; }
+        public string Category { get; set; }
+        public string ImageSlug { get; set; }
+        public string ImageUrl { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Synopsis { get; set; }
+        public string Slug { get; set; }
+        public Guid? UserId { get; set; }
+    }
+
+    public class BookVM
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string PhoneDonor { get; set; }
+        public string Status { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? ChooseDate { get; set; }
+        public string FreightOption { get; set; }
+        public Guid CategoryId { get; set; }
+        public string Category { get; set; }
+        public string ImageSlug { get; set; }
+        public string ImageUrl { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Synopsis { get; set; }
+        public string Slug { get; set; }
+        public Guid? UserId { get; set; }
     }
 }
