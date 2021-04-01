a94e3da3ea3d77726fbd6c9748e51fac14b8700f
Marcelo Antunes <mrcelo.antunes@gmail.com>
2021-04-01 11:51:02 -0300

    bugfix: Corrige erro de serialização da resposta do endpoint Cancel

---
+++ ShareBook/ShareBook.Api/ViewModels/CancelBookDonationVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShareBook.Api.ViewModels
+{
+    public class CancelBookDonationVM
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Status { get; set; }
+    }
+}
