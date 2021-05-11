9394f88b40c30a01b691ece369936ddc11a2a115
henriqueholtz <henrique_holtz@hotmail.com>
2021-05-11 20:56:37 -0300

    Added Migration 'AddAttributeInTable_EbookComplaints_ReasonMessage'

---
+++ ShareBook/ShareBook.Repository/Migrations/20210511235022_AddAttributeInTable_EbookComplaints_ReasonMessage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShareBook.Repository.Migrations
+{
+    public partial class AddAttributeInTable_EbookComplaints_ReasonMessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReasonMessage",
+                table: "EbookComplaints",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReasonMessage",
+                table: "EbookComplaints");
+        }
+    }
+}
