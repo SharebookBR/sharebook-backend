c842521647038ed4cf725ad6c98e55aa80ab5895
henriqueholtz <henrique_holtz@hotmail.com>
2021-05-11 19:31:29 -0300

    Add Migration 'AddAttributeInTable_Book_QuantityValidations'

---
+++ ShareBook/ShareBook.Repository/Migrations/20210511222838_AddAttributeInTable_Book_QuantityValidations.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShareBook.Repository.Migrations
+{
+    public partial class AddAttributeInTable_Book_QuantityValidations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "QuantityValidatons",
+                table: "Books",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuantityValidatons",
+                table: "Books");
+        }
+    }
+}
