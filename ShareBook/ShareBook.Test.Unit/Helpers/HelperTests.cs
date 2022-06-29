60efb55cd3e93fd10e917ffc85c22e6ef6cff683
Diego Fernandes <fernandes.diego_@hotmail.com>
2022-06-29 12:29:37 -0300

    Add helper para redimensionar imagens usando ImageSharp

--- ShareBook/ShareBook.Test.Unit/Helpers/HelperTests.cs
+++ ShareBook/ShareBook.Test.Unit/Helpers/HelperTests.cs
@@ -1,6 +1,7 @@
 ﻿using ShareBook.Helper.Extensions;
 using ShareBook.Helper.Image;
 using Xunit;
+using Flurl.Http;
 
 namespace ShareBook.Test.Unit.Helpers
 {
@@ -137,6 +138,16 @@ namespace ShareBook.Test.Unit.Helpers
             result = Helper.ClientVersionValidation.IsValidVersion("v0.20.0", minVersion);
             Assert.True(result);
 
+        }
+        
+        [Fact]
+        public void ImageResize()
+        {
+            var imageurl = "https://images.sympla.com.br/62b34c1818c0f.png";
+
+            var imageBytes = imageurl.GetBytesAsync().Result;
+            var result = ImageHelper.ResizeImage(imageBytes, 50);
+            Assert.Equal(typeof(byte[]), result.GetType());
         }
     }
 }
