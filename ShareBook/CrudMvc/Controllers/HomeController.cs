a7d2850c6faa5c43cc8f47cb09343ef609afd18a
Raffaello Damgaard <raffaello.neves@mc1global.com>
2022-11-12 14:01:07 -0300

    Easy data poc.

---
+++ ShareBook/CrudMvc/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using CrudMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using ShareBook.Repository;
+using System.Diagnostics;
+
+namespace CrudMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext _ctx;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext ctx)
+        {
+            _logger = logger;
+            _ctx = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Raffa()
+        {
+            var meetups = _ctx.Meetups.ToList();
+            return Json(meetups);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
