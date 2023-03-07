f49b2129e3c46c4e6252b9e49fe4db3d92de2e9e
Raffaello Damgaard <raffaello_neves_ext@carrefour.com>
2023-03-07 14:31:39 -0300

    Bugfix: erro de duplicidade no pushnotification.

--- ShareBook/ShareBook.Service/PushNotification/PushNotificationService.cs
+++ ShareBook/ShareBook.Service/PushNotification/PushNotificationService.cs
@@ -7,105 +7,112 @@ using ShareBook.Domain;
 using ShareBook.Domain.Enums;
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
-namespace ShareBook.Service.Notification
+namespace ShareBook.Service.Notification;
+
+public class PushNotificationService : IPushNotificationService
 {
-    public class PushNotificationService : IPushNotificationService
+    private readonly PushNotificationSettings _settings;
+    private readonly OneSignalClient _oneSignalClient;
+    public PushNotificationService(IOptions<PushNotificationSettings> pushNotificationSettings)
     {
-        private readonly PushNotificationSettings _settings;
-        private readonly OneSignalClient _oneSignalClient;
-        private readonly NotificationCreateOptions _notificationCreateOptions;
-        public PushNotificationService(IOptions<PushNotificationSettings> pushNotificationSettings)
+        _settings = pushNotificationSettings.Value;
+        _oneSignalClient = new OneSignalClient(_settings.ApiKey);
+    }
+
+    public string SendNotificationSegments(NotificationOnesignal onesignal)
+    {
+        if (!_settings.IsActive) return "";
+
+        var notificationCreateOptions = new NotificationCreateOptions
         {
-            _settings = pushNotificationSettings.Value;
-            _oneSignalClient = new OneSignalClient(_settings.ApiKey);
-            _notificationCreateOptions = new NotificationCreateOptions
+            AppId = new Guid(_settings.AppId)
+        };
+
+        notificationCreateOptions.IncludedSegments = new List<string>()
+        {
+            GetSegments(onesignal.TypeSegments)
+        };
+
+        notificationCreateOptions.Headings.Add(LanguageCodes.Portuguese, onesignal.Title);
+        notificationCreateOptions.Contents.Add(LanguageCodes.Portuguese, onesignal.Content);
+
+        _oneSignalClient.Notifications.Create(notificationCreateOptions);
+
+        return "Enviado com sucesso";
+    }
+
+    public string SendNotificationByKey(NotificationOnesignal onesignal)
+    {
+        if (!_settings.IsActive) return "";
+
+        var notificationCreateOptions = new NotificationCreateOptions
+        {
+            AppId = new Guid(_settings.AppId)
+        };
+
+        notificationCreateOptions.Filters = new List<INotificationFilter>
+        {
+            new NotificationFilterField { Field = NotificationFilterFieldTypeEnum.Tag, Key = onesignal.Key, Value = onesignal.Value}
+        };
+
+        notificationCreateOptions.Headings.Add(LanguageCodes.English, onesignal.Title);
+        notificationCreateOptions.Contents.Add(LanguageCodes.English, onesignal.Content);
+
+        notificationCreateOptions.Headings.Add(LanguageCodes.Portuguese, onesignal.Title);
+        notificationCreateOptions.Contents.Add(LanguageCodes.Portuguese, onesignal.Content);
+
+        _oneSignalClient.Notifications.Create(notificationCreateOptions);
+
+        return $"Notification enviado para o {onesignal.Value} com sucesso";
+    }
+
+
+    public string SendNotificationByEmail(string email, string title, string content)
+    {
+        if (!_settings.IsActive) return "";
+
+        try {
+            var notificationCreateOptions = new NotificationCreateOptions
             {
                 AppId = new Guid(_settings.AppId)
             };
-        }
 
-        public string SendNotificationSegments(NotificationOnesignal onesignal)
-        {
-            // TODO: verificar se esse serviço está ativo no appsettings.
-
-            _notificationCreateOptions.IncludedSegments = new List<string>()
+            notificationCreateOptions.Filters = new List<INotificationFilter>
             {
-                GetSegments(onesignal.TypeSegments)
+                new NotificationFilterField { Field = NotificationFilterFieldTypeEnum.Tag, Key = "email", Value = email}
             };
 
-            _notificationCreateOptions.Headings.Add(LanguageCodes.Portuguese, onesignal.Title);
-            _notificationCreateOptions.Contents.Add(LanguageCodes.Portuguese, onesignal.Content);
+            notificationCreateOptions.Headings.Add(LanguageCodes.English, title);
+            notificationCreateOptions.Contents.Add(LanguageCodes.English, content);
 
-            _oneSignalClient.Notifications.Create(_notificationCreateOptions);
+            notificationCreateOptions.Headings.Add(LanguageCodes.Portuguese, title);
+            notificationCreateOptions.Contents.Add(LanguageCodes.Portuguese, content);
 
-            return "Enviado com sucesso";
+            _oneSignalClient.Notifications.Create(notificationCreateOptions);
+
+            return $"Notification enviado para o {email} com sucesso";
         }
+        catch(Exception ex) {
+            RollbarLocator.RollbarInstance.Error(ex);
+            return "";
+        }            
+    }
 
-        public string SendNotificationByKey(NotificationOnesignal onesignal)
+    private string GetSegments(TypeSegments typeSegments)
+    {
+        switch (typeSegments)
         {
-            // TODO: verificar se esse serviço está ativo no appsettings.
-
-            _notificationCreateOptions.Filters = new List<INotificationFilter>
-            {
-                new NotificationFilterField { Field = NotificationFilterFieldTypeEnum.Tag, Key = onesignal.Key, Value = onesignal.Value}
-            };
-
-            _notificationCreateOptions.Headings.Add(LanguageCodes.English, onesignal.Title);
-            _notificationCreateOptions.Contents.Add(LanguageCodes.English, onesignal.Content);
-
-            _notificationCreateOptions.Headings.Add(LanguageCodes.Portuguese, onesignal.Title);
-            _notificationCreateOptions.Contents.Add(LanguageCodes.Portuguese, onesignal.Content);
-
-            _oneSignalClient.Notifications.Create(_notificationCreateOptions);
-
-
-            return $"Notification enviado para o {onesignal.Value} com sucesso";
-        }
-
-
-        public string SendNotificationByEmail(string email, string title, string content)
-        {
-            if (!_settings.IsActive) return "";
-
-            try {
-                _notificationCreateOptions.Filters = new List<INotificationFilter>
-                {
-                        new NotificationFilterField { Field = NotificationFilterFieldTypeEnum.Tag, Key = "email", Value = email}
-                };
-
-                _notificationCreateOptions.Headings.Add(LanguageCodes.English, title);
-                _notificationCreateOptions.Contents.Add(LanguageCodes.English, content);
-
-                _notificationCreateOptions.Headings.Add(LanguageCodes.Portuguese, title);
-                _notificationCreateOptions.Contents.Add(LanguageCodes.Portuguese, content);
-
-                _oneSignalClient.Notifications.Create(_notificationCreateOptions);
-
-                return $"Notification enviado para o {email} com sucesso";
-            }
-            catch(Exception ex) {
-                RollbarLocator.RollbarInstance.Error(ex);
+            case TypeSegments.Inactive:
+                return "Inactive Users";
+            case TypeSegments.Engaged:
+                return "Engaged Users";
+            case TypeSegments.All:
+                return "Subscribed Users";
+            case TypeSegments.Active:
+                return "Active Users";
+            default:
                 return "";
-            }            
-        }
-
-        private string GetSegments(TypeSegments typeSegments)
-        {
-            switch (typeSegments)
-            {
-                case TypeSegments.Inactive:
-                    return "Inactive Users";
-                case TypeSegments.Engaged:
-                    return "Engaged Users";
-                case TypeSegments.All:
-                    return "Subscribed Users";
-                case TypeSegments.Active:
-                    return "Active Users";
-                default:
-                    return "";
-            }
         }
     }
 }
