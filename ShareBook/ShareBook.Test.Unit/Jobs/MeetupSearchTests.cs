02abafc18e4dfe674edf796b8a9a1f06503e8812
Henrique Holtz <henrique_holtz@hotmail.com>
2024-06-10 19:45:04 -0300

    test: Jobs.MeetupSearchTests => Adding first test "MeetupSettingsDisabled_ShouldReturn_MeetupDisabled"

---
+++ ShareBook/ShareBook.Test.Unit/Jobs/MeetupSearchTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using Sharebook.Jobs;
+using ShareBook.Service;
+using System.Threading.Tasks;
+using Xunit;
+using ShareBook.Repository;
+using Microsoft.Extensions.Configuration;
+using ShareBook.Domain.Enums;
+
+namespace ShareBook.Test.Unit.Jobs
+{
+    public class MeetupSearchTests
+    {
+        private readonly Mock<IJobHistoryRepository> _mockJobHistoryRepository = new();
+        private readonly Mock<IMeetupService> _mockMeetupService = new();
+        private readonly Mock<IConfiguration> _mockConfiguration = new();
+
+        [Fact]
+        public async Task MeetupSettingsDisabled_ShouldReturn_MeetupDisabled()
+        {
+            _mockConfiguration.SetupGet(s => s[It.IsAny<string>()]).Returns("false");
+            MeetupSearch job = new MeetupSearch(_mockJobHistoryRepository.Object, _mockMeetupService.Object, _mockConfiguration.Object);
+
+            JobResult result = await job.ExecuteAsync();
+            Assert.Equal(JobResult.MeetupDisabled, result);
+        }
+    }
+}
