b6e9ac0085c03b2512df939d85aa5c8c46d4439d
Henrique Holtz <henrique_holtz@hotmail.com>
2024-06-14 07:40:38 -0300

    test: Jobs.CancelAbandonedDonations => Adding unit test to make sure is not cancelling books which are not to be cancelled

--- ShareBook/ShareBook.Test.Unit/Jobs/0 - CancelAbandonedDonationsTests.cs
+++ ShareBook/ShareBook.Test.Unit/Jobs/0 - CancelAbandonedDonationsTests.cs
@@ -12,6 +12,7 @@ using ShareBook.Test.Unit.Mocks;
 using ShareBook.Domain.Common;
 using System.Linq;
 using System;
+using Xunit.Extensions.Ordering;
 
 namespace ShareBook.Test.Unit.Jobs
 {
@@ -21,29 +22,47 @@ namespace ShareBook.Test.Unit.Jobs
         private readonly Mock<IBookService> _mockBookService = new();
         private readonly Mock<IBookUserService> _mockBookUserService = new();
         private readonly Mock<IConfiguration> _mockConfiguration = new();
-
-        public CancelAbandonedDonationsTests()
-        {
-            // Mocking 4 books. 2 Of them are ready to be cancelled.
-            int maxLateDonationDaysAutoCancel = 90;
-            List<Book> allBooks = new List<Book> {
+        private static int _maxLateDonationDaysAutoCancel = 90;
+        private static List<Book> _allBooks = new List<Book> {
                     BookMock.GetLordTheRings(),
                     BookMock.GetLordTheRings(),
                     BookMock.GetLordTheRings(),
                     BookMock.GetLordTheRings(),
                 };
-            var booksToCancel = allBooks.Take(2);
-            foreach (var book in booksToCancel) 
-                book.ChooseDate = DateTime.Now.AddDays((maxLateDonationDaysAutoCancel + 2) * -1);
 
-            _mockConfiguration.SetupGet(s => s[It.IsAny<string>()]).Returns(maxLateDonationDaysAutoCancel.ToString());
-            _mockBookService.Setup(s => s.GetBooksChooseDateIsLateAsync()).ReturnsAsync(allBooks);
+        public CancelAbandonedDonationsTests()
+        {
+
+            _mockConfiguration.SetupGet(s => s[It.IsAny<string>()]).Returns(_maxLateDonationDaysAutoCancel.ToString());
+            _mockBookService.Setup(s => s.GetBooksChooseDateIsLateAsync()).ReturnsAsync(_allBooks);
             _mockBookUserService.Setup(s => s.CancelAsync(It.IsAny<BookCancelationDTO>())).ReturnsAsync(() => new Result<Book>(BookMock.GetLordTheRings()));
         }
 
-        [Fact]
+        [Fact, Order(1)]
+        public async Task NotCancellingAnyBook()
+        {
+            CancelAbandonedDonations job = new CancelAbandonedDonations(_mockJobHistoryRepository.Object, _mockBookService.Object, _mockBookUserService.Object, _mockConfiguration.Object);
+
+            JobHistory result = await job.WorkAsync();
+
+            Assert.True(result.IsSuccess);
+            Assert.Equal("Encontradas 0 doações abandonadas com mais de 90 dias de atraso.\n\n", result.Details);
+            Assert.Equal(CancelAbandonedDonations.JobName, result.JobName);
+
+            _mockBookService.Verify(c => c.GetBooksChooseDateIsLateAsync(), Times.Once);
+            _mockBookUserService.Verify(c => c.CancelAsync(It.IsAny<BookCancelationDTO>()), Times.Never);
+            _mockBookService.VerifyNoOtherCalls();
+            _mockBookUserService.VerifyNoOtherCalls();
+        }
+
+        [Fact, Order(2)]
         public async Task Cancelling2Of4AbandonedBooks()
         {
+            // Mocking 4 books. 2 Of them are ready to be cancelled.
+            var booksToCancel = _allBooks.Take(2);
+            foreach (var book in booksToCancel)
+                book.ChooseDate = DateTime.Now.AddDays((_maxLateDonationDaysAutoCancel + 2) * -1);
+
             CancelAbandonedDonations job = new CancelAbandonedDonations(_mockJobHistoryRepository.Object, _mockBookService.Object, _mockBookUserService.Object, _mockConfiguration.Object);
             
             JobHistory result = await job.WorkAsync();
