19386b0ef778f1174ba1c856b0f7b1e397697900
Henrique Holtz <henrique_holtz@hotmail.com>
2024-06-15 07:23:41 -0300

    fix: UnitOfWork => Avoiding exception when is called "DisposeAsync"

--- ShareBook/ShareBook.Repository/UoW/UnitOfWork.cs
+++ ShareBook/ShareBook.Repository/UoW/UnitOfWork.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace ShareBook.Repository.UoW
@@ -12,6 +13,10 @@ namespace ShareBook.Repository.UoW
         public async Task BeginTransactionAsync() => await _context.Database.BeginTransactionAsync();
         public async Task CommitAsync() => await _context.Database.CommitTransactionAsync();
         public async Task RollbackAsync() => await _context.Database.RollbackTransactionAsync();
-        public async ValueTask DisposeAsync() => await _context.Database.CurrentTransaction?.RollbackAsync();
+        public async ValueTask DisposeAsync()
+        {
+            if (_context?.Database?.CurrentTransaction != null)
+                await _context.Database.CurrentTransaction.RollbackAsync();
+        }
     }
 }
