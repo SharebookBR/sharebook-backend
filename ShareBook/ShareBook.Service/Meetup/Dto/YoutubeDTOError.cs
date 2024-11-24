ab007d4d321bcc31437203bc7fabdfdf911ac9e0
Raffaello Damgaard <raffaello_neves@carrefour.com>
2024-11-24 20:17:02 -0300

    Busca meetps apenas do You Tube.

---
+++ ShareBook/ShareBook.Service/Meetup/Dto/YoutubeDTOError.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ShareBook.Service.Dto;
+
+public class YoutubeDtoError
+{
+    public Error error { get; set; }
+}
+
+public class Error
+{
+    public int Code { get; set; }
+    public string Message { get; set; }
+    public List<YoutubeErrorDetailDto> Errors { get; set; }
+    public string Status { get; set; }
+}
+
+public class YoutubeErrorDetailDto
+{
+    public string Message { get; set; }
+    public string Domain { get; set; }
+    public string Reason { get; set; }
+}
