ffbde9a04a788d474ab3f2039b20bb86df587040
Raffaello Damgaard <raffaello_neves@carrefour.com>
2024-12-21 19:12:29 -0300

    Obter detalhes do vídeo no You Tube

---
+++ ShareBook/ShareBook.Service/Meetup/Dto/YoutubeDetailDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShareBook.Service.Dto;
+
+public class YoutubeDtoDetail
+{
+    public string nextPageToken { get; set; }
+    public string prevPageToken { get; set; }
+    public PageInfo pageInfo { get; set; }
+    public List<ItemDetail> Items { get; set; }
+    
+}
+
+public class ItemDetail
+{
+    public String Id { get; set; }
+    public Snippet Snippet { get; set; }
+    public liveStreamingDetails liveStreamingDetails { get; set; }
+}
+
+public class liveStreamingDetails
+{
+    public DateTime scheduledStartTime { get; set; }
+    public string activeLiveChatId { get; set; }
+}
