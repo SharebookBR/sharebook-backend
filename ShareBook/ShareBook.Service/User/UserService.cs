da81718ea87f47028aa6dd2d22e41d3e1886b6cc
Raffaello Damgaard <raffaello_neves@carrefour.com>
2025-09-20 20:31:32 -0300

    GetFacilitators compatível com postgres e sqlite.

--- ShareBook/ShareBook.Service/User/UserService.cs
+++ ShareBook/ShareBook.Service/User/UserService.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using ShareBook.Domain;
 using ShareBook.Domain.Common;
 using ShareBook.Domain.DTOs;
@@ -17,6 +13,11 @@ using ShareBook.Repository.Repository;
 using ShareBook.Repository.UoW;
 using ShareBook.Service.Generic;
 using ShareBook.Service.Recaptcha;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ShareBook.Service
 {
@@ -26,7 +27,8 @@ namespace ShareBook.Service
         private readonly IBookRepository _bookRepository;
         private readonly IUserEmailService _userEmailService;
         private readonly IRecaptchaService _recaptchaService;
-        
+        private readonly IConfiguration _config;
+
         private readonly IMapper _mapper;
 
 
@@ -37,13 +39,14 @@ namespace ShareBook.Service
             IValidator<User> validator,
             IMapper mapper,
             IUserEmailService userEmailService,
-            IRecaptchaService recaptchaService) : base(userRepository, unitOfWork, validator)
+            IRecaptchaService recaptchaService, IConfiguration config) : base(userRepository, unitOfWork, validator)
         {
             _userRepository = userRepository;
             _userEmailService = userEmailService;
             _bookRepository = bookRepository;
             _mapper = mapper;
             _recaptchaService = recaptchaService;
+            _config = config;
         }
 
         public async Task<Result<User>> AuthenticationByEmailAndPasswordAsync(User user)
@@ -99,7 +102,7 @@ namespace ShareBook.Service
             Result resultRecaptcha = _recaptchaService.SimpleValidationRecaptcha(userDto?.RecaptchaReactive);
 
             var result = await ValidateAsync(user);
-            if (!resultRecaptcha.Success && resultRecaptcha.Messages != null) 
+            if (!resultRecaptcha.Success && resultRecaptcha.Messages != null)
                 result.Messages.AddRange(resultRecaptcha.Messages);
 
             if (!result.Success)
@@ -232,31 +235,53 @@ namespace ShareBook.Service
 
         public IList<User> GetFacilitators(Guid userIdDonator)
         {
-            string query = @"SELECT
-                            CONCAT(Name, ' (', total, ')') as Name, Id
-                            FROM
-                            (
-                                SELECT TOP 100
-                                    u.Name, u.Id,
-                                    (SELECT COUNT(*) as total FROM Books b
-                                      WHERE b.UserIdFacilitator = u.Id and b.UserId = {0}
-                                    ) as total
-                                FROM
-                                    Users u
-                                WHERE u.Profile = 0 -- Administrador
-                                ORDER BY total desc, u.Name
-                            ) sub";
+            var database = _config["DatabaseProvider"].ToLower();
+
+            string query;
+
+            if (database == "sqlserver")
+            {
+                query = @"
+                    SELECT TOP 100
+                        u.Id,
+                        CONCAT(u.Name, ' (', COUNT(b.Id), ')') as Name
+                    FROM Users u
+                    LEFT JOIN Books b
+                        ON b.UserIdFacilitator = u.Id
+                       AND b.UserId = {0}
+                    WHERE u.Profile = 0
+                    GROUP BY u.Id, u.Name
+                    ORDER BY COUNT(b.Id) DESC, u.Name";
+            }
+            else // Postgres ou SQLite
+            {
+                query = @"
+                    SELECT
+                        u.""Id"",
+                        (u.""Name"" || ' (' || COUNT(b.""Id"") || ')') as ""Name""
+                    FROM ""Users"" u
+                    LEFT JOIN ""Books"" b
+                        ON b.""UserIdFacilitator"" = u.""Id""
+                       AND b.""UserId"" = {0}
+                    WHERE u.""Profile"" = 0
+                    GROUP BY u.""Id"", u.""Name""
+                    ORDER BY COUNT(b.""Id"") DESC, u.""Name""
+                    LIMIT 100";
+            }
+
             var parameters = new object[] { userIdDonator };
 
             return _repository.FromSql(query, parameters)
-                    .Select(x => new User
-                    {
-                        Id = x.Id,
-                        Name = x.Name
-                    }).ToList();
+                .Select(x => new User
+                {
+                    Id = x.Id,
+                    Name = x.Name
+                }).ToList();
         }
 
-        public IList<User> GetAdmins() {
+
+        public IList<User> GetAdmins()
+        {
             return _userRepository.Get()
                 .Where(u => u.Profile == Domain.Enums.Profile.Administrator)
                 .ToList();
@@ -346,7 +371,7 @@ namespace ShareBook.Service
             await _userEmailService.SendEmailParentAprovedNotifyUserAsync(user);
         }
 
-        
+
         #endregion Private
     }
 }
