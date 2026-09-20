0977947ec11278ddde0d61a927db58fe7ca338c5
Raffaello Damgaard <raffacabofrio@gmail.com>
2026-09-20 21:18:12 +0000

    Email: mapeia payload camelCase do webhook Stalwart

--- ShareBook/ShareBook.Api/ViewModels/StalwartWebhookVM.cs
+++ ShareBook/ShareBook.Api/ViewModels/StalwartWebhookVM.cs
@@ -1,17 +1,26 @@
 using System.Collections.Generic;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ShareBook.Api.ViewModels;
 
 public class StalwartWebhookVM
 {
+    [JsonPropertyName("events")]
     public IList<StalwartWebhookEventVM> Events { get; set; } = new List<StalwartWebhookEventVM>();
 }
 
 public class StalwartWebhookEventVM
 {
+    [JsonPropertyName("id")]
     public string Id { get; set; } = string.Empty;
+
+    [JsonPropertyName("createdAt")]
     public string CreatedAt { get; set; } = string.Empty;
+
+    [JsonPropertyName("type")]
     public string Type { get; set; } = string.Empty;
+
+    [JsonPropertyName("data")]
     public JsonElement Data { get; set; }
 }
